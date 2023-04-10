0063c7957a5e743fd2ed2b68196957e0af2f2a66
Saurabh0713 <109545639+Saurabh0713@users.noreply.github.com>
2023-04-10 22:18:49 +0530

    update

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,6 +6,8 @@ import njs from '../Image/logo/node-js.webp'
 import r from '../Image/logo/r.png'
 import t from '../Image/logo/tw.png'
 import g from '../Image/logo/g.png'
+import sass from '../Image/logo/sass.png'
+import jq from '../Image/logo/jq.png'
 
 export default function About() {
   return (
@@ -14,7 +16,7 @@ export default function About() {
       <div className='bg-neutral-100 rounded-xl drop-shadow-xl p-4 w-1/2 text-center max-md:h-1/2 max-md:w-full'>
         <h2 className='text-5xl my-3'>About me</h2>
         <p>Innovative, task driven professional in web design and development. Proficient in developing web pages, creating user interfaces, writing and testing codes, troubleshooting simple/complex issues and implementing new features.</p>
-        <p>Technical Skills - React Js, Java Script, Html5, Css3, Jsx, Bootstrap, Tailwind, Figma</p>
+        <p>Technical Skills - React Js, Java Script, jQuery, Html, Css, Sass, Jsx, Bootstrap, Tailwind, Figma</p>
       </div>
         <div className='flex justify-center items-center flex-wrap h-56 max-md:py-4'>
         <img className='w-24 max-md:w-16 m-2 animate-spin' src={r}></img>
@@ -24,6 +26,8 @@ export default function About() {
         <img className='w-24 max-md:w-16 m-2 animate-bounce' src={njs}></img>
         <img className='w-24 max-md:w-16 m-2 animate-pulse' src={t}></img>
         <img className='w-24 max-md:w-16 m-2 animate-pulse' src={g}></img>
+        <img className='w-24 max-md:w-16 m-2 animate-pulse' src={sass}></img>
+        <img className='w-24 max-md:w-16 m-2 animate-bounce' src={jq}></img>
         </div>
       </div>
     </div>
